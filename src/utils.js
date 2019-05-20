22346e3778b4a879cde48ab3c084e8bbc5c3b2dc
Mario Sánchez <msanchez@trovit.com>
2019-05-20 20:02:37 +0200

    replacing lodash get with a tinny native version

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,17 @@
 import _clone from 'lodash.clone';
-import _get from 'lodash.get';
 import _setWith from 'lodash.setwith';
 
+const get = (obj, path, defaultValue = null) =>
+  String.prototype.split
+    .call(path, /[,[\].]+?/)
+    .filter(Boolean)
+    .reduce(
+      (a, c) => (Object.hasOwnProperty.call(a, c) ? a[c] : defaultValue),
+      obj
+    );
+
 export const getIn = (state, path, defaultValue) => {
-  return _get(state, path, defaultValue);
+  return get(state, path, defaultValue);
 };
 
 export const setIn = (state, path, valueToSet) => {
