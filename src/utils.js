c955dbd7340e9d4d0b62c1fcf402da3ee84cfdcb
Mario Sánchez <msanchez@trovit.com>
2019-05-24 18:03:29 +0200

    removing lodash clone dependency making the bundle even smaller

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,32 @@
-import _clone from 'lodash.clone';
 import _setWith from 'lodash.setwith';
 
-const get = (obj, path, defaultValue = null) =>
-  String.prototype.split
+function get(obj, path, defaultValue = null) {
+  return String.prototype.split
     .call(path, /[,[\].]+?/)
     .filter(Boolean)
     .reduce(
       (a, c) => (Object.hasOwnProperty.call(a, c) ? a[c] : defaultValue),
       obj
     );
+}
+
+function deepClone(obj) {
+  const clone = Object.assign({}, obj);
+  Object.keys(clone).forEach(
+    key =>
+      (clone[key] =
+        typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key])
+  );
+  if (Array.isArray(obj) && obj.length) {
+    return (clone.length = obj.length) && Array.from(clone);
+  }
+  return Array.isArray(obj) ? Array.from(obj) : clone;
+}
 
 export const getIn = (state, path, defaultValue) => {
   return get(state, path, defaultValue);
 };
 
 export const setIn = (state, path, valueToSet) => {
-  return _setWith({ ...state }, path, valueToSet, nsValue => {
-    return _clone(nsValue);
-  });
+  return _setWith({ ...state }, path, valueToSet, deepClone);
 };
