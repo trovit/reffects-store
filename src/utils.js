ace16b9ff97f83f3453ef23e630481028355434a
Mario Sánchez <msanchez@trovit.com>
2019-05-26 01:34:22 +0200

    removing setwith lodash dependency, now the library has 0 dependencies

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,9 @@
-import _setWith from 'lodash.setwith';
+function getPathArray(path) {
+  return Array.isArray(path) ? path : path.split('.');
+}
 
 function get(obj, path, defaultValue = null) {
-  return String.prototype.split
-    .call(path, /[,[\].]+?/)
+  return getPathArray(path)
     .filter(Boolean)
     .reduce(
       (a, c) => (Object.hasOwnProperty.call(a, c) ? a[c] : defaultValue),
@@ -10,23 +11,23 @@ function get(obj, path, defaultValue = null) {
     );
 }
 
-function deepClone(obj) {
-  const clone = Object.assign({}, obj);
-  Object.keys(clone).forEach(
-    key =>
-      (clone[key] =
-        typeof obj[key] === 'object' ? deepClone(obj[key]) : obj[key])
-  );
-  if (Array.isArray(obj) && obj.length) {
-    return (clone.length = obj.length) && Array.from(clone);
-  }
-  return Array.isArray(obj) ? Array.from(obj) : clone;
-}
-
 export const getIn = (state, path, defaultValue) => {
   return get(state, path, defaultValue);
 };
 
 export const setIn = (state, path, valueToSet) => {
-  return _setWith({ ...state }, path, valueToSet, deepClone);
+  return set({ ...state }, path, valueToSet);
 };
+
+function set(obj, path, newValue) {
+  if (path.length === 0) {
+    return obj;
+  }
+  return getPathArray(path).reduceRight(
+    (value, currentPathKey, index, pathKeys) => ({
+      ...get(obj, pathKeys.slice(0, index)),
+      [currentPathKey]: value,
+    }),
+    newValue
+  );
+}
